cc4ed52f0ce660f0b21dc2f0c30b913b8d7d41cf
RTUnreal <unreal@rtinf.net>
2024-05-16 23:50:47 +0200

    make some changes to steam

--- 2configs/steam.nix
+++ 2configs/steam.nix
@@ -10,31 +10,57 @@
     unset QT_PLUGIN_PATH
     echo "$@" | sed "s|'$HOME/.local/share/Steam/steamapps/common/OVR_AdvancedSettings/run.sh'|appimage-run $HOME/.local/share/Steam/steamapps/common/OVR_AdvancedSettings/OVRAS.AppImage|"|bash -
   '';
+
+  # copied from: https://www.gamingonlinux.com/forum/topic/5716/
+  clear_cache = pkgs.writeShellApplication {
+    name = "steam_clear_shader_caches";
+    text = ''
+      ### Clean out ~/.cache/mesa_shader_cache(_sf) and radv_builtin shaders
+      ### Clean out the mesa_shader_cache(_sf) directories from Steam's compatdata
+      ### Note that the fossilize cache (MESA_DISK_CACHE_SINGLE_FILE=1) isn't used by default,
+      ### so we cover the mesa_shader_cache_sf case with a wildcard
+
+      # System specific path, you may need to edit below if the following doesn't lead
+      # to the real path correctly
+
+      cd "$HOME"/.steam/root/steamapps/compatdata || exit 1
+      COMPATDATA=$(pwd -P)
+
+      find "$COMPATDATA" -name 'mesa_shader_cache*' -exec du -sh '{}' \;
+      find "$HOME"/.cache -name 'mesa_shader_cache*' -exec du -sh '{}' \;
+
+      echo "Look at all the CRUD that is accumulating..."
+
+      while true; do
+      read -rp "Do you wish to clean it out? (y/n) " yn
+      case $yn in
+              [yY] ) echo Nuking...;
+                      break;;
+              [nN] ) echo Aborting...;
+                      exit 0;;
+              * ) echo "Enter y or n, silly!";;
+      esac
+      done
+
+      rm -vf "$HOME"/.cache/radv_builtin_shaders32 || true
+      rm -vf "$HOME"/.cache/radv_builtin_shaders64 || true
+      rm -rvf "$HOME"/.cache/mesa_shader_cache* || true
+      find "$COMPATDATA" -name 'mesa_shader_cache*' -exec rm -rvf '{}' + || true
+    '';
+  };
 in {
   programs = {
     steam = {
       enable = true;
       package = pkgs.steam.override {
-        extraLibraries = pkgs:
-          with config.hardware.opengl;
-            (
-              if pkgs.hostPlatform.is64bit
-              then [package] ++ extraPackages
-              else [package32] ++ extraPackages32
-            )
-            ++ [pkgs.appimage-run ovrasStarter];
+        extraLibraries = pkgs: [pkgs.appimage-run ovrasStarter];
       };
       extraCompatPackages = [nixpkgs-unstable.proton-ge-bin];
       platformOptimizations.enable = true;
     };
-    # XXX: rethink gamescop
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
     gamemode.enable = true;
   };
   environment = {
-    systemPackages = [ovrasStarter];
+    systemPackages = [ovrasStarter clear_cache];
   };
 }
