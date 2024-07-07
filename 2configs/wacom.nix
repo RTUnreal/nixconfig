286846069c045b84067c43b46a402b2af274046b
RTUnreal <unreal@rtinf.net>
2024-07-07 11:39:33 +0200

    wacom: dont include pkgs.wacomtablet when kde is enabled to dedup

--- 2configs/wacom.nix
+++ 2configs/wacom.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
-  services.xserver.wacom.enable = true;
-  environment.systemPackages = with pkgs; [wacomtablet];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf mkMerge;
+in {
+  config = mkMerge [
+    {
+      services.xserver.wacom.enable = true;
+    }
+    (mkIf (!config.services.desktopManager.plasma6.enable) {
+      environment.systemPackages = [pkgs.wacomtablet];
+    })
+  ];
 }
