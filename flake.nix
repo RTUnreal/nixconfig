70eea1444df0d106afc8f6db6893feb50f56af3f
RTUnreal <unreal@rtinf.net>
2024-07-13 00:32:00 +0200

    switch to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     retiolum.url = "git+https://git.thalheim.io/Mic92/retiolum";
     flake-compat = {
       url = "github:edolstra/flake-compat";
