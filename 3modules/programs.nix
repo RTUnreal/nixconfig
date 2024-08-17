82611a66e9936011d39e14384ace5de19b57d84a
RTUnreal <unreal@rtinf.net>
2024-08-17 12:03:17 +0200

    3modules/programs: fix mpv

--- 3modules/programs.nix
+++ 3modules/programs.nix
@@ -1,15 +1,18 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }: let
   inherit (lib) mkMerge mkIf mkEnableOption;
+
+  cfg = config.rtinf.programs;
 in {
   options.rtinf.programs = {
     mpv = mkEnableOption "mpv";
   };
   config = mkMerge [
-    (mkIf false {
+    (mkIf cfg.mpv {
       nixpkgs.overlays = [
         (final: prev: {
           mpv = prev.mpv.override {
