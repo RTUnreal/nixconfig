4c9e14d335b2f4e4d2d04c361c285ca292d850c1
RTUnreal <unreal@rtinf.net>
2024-09-24 16:05:10 +0200

    chore: update slimevr appimage

--- 5pkgs/slimevr/appimage.nix
+++ 5pkgs/slimevr/appimage.nix
@@ -4,13 +4,13 @@
   fetchurl,
 }: let
   pname = "slimevr";
-  version = "0.12.1";
+  version = "0.13.0";
 in
   appimageTools.wrapType2 rec {
     inherit pname version;
     src = fetchurl {
       url = "https://github.com/SlimeVR/SlimeVR-Server/releases/download/v${version}/SlimeVR-amd64.appimage";
-      sha256 = "sha256-AyXL1oVmbEmGbAXQT4cWKvAHM+fkK2DfMSrizwuYRbU=";
+      sha256 = "sha256-s5HMsiTSmPp1x0WTPZnn85NnYdOCuO5zTFGd0XqlEIc=";
     };
 
     extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ [pkgs.libthai pkgs.jdk17_headless];
