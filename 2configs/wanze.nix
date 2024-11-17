762e0b8d5a8a315bd7561ce097c699729a51bf1d
RTUnreal <unreal@rtinf.net>
2024-11-18 00:32:35 +0100

    2configs/wanze: first attempt to add voice assistant

--- 2configs/wanze.nix
+++ 2configs/wanze.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib, ...}: {
   services.home-assistant = {
     enable = true;
     openFirewall = true;
@@ -11,7 +11,43 @@
       # added modules
       "dwd_weather_warnings"
       "mobile_app"
+
+      "assist_pipeline"
+      "wyoming"
+      "wake_word"
     ];
-    config = {};
+    config = {
+      mobile_app = {};
+      assist_pipeline = {};
+      wake_word = {};
+    };
   };
+
+  services.wyoming.piper = {
+    servers = {
+      "en" = {
+        enable = true;
+        # see https://github.com/rhasspy/rhasspy3/blob/master/programs/tts/piper/script/download.py
+        voice = "de-eva_k-x-low"; #"en-gb-southern_english_female-low";
+        uri = "tcp://0.0.0.0:10200";
+        speaker = 0;
+      };
+    };
+  };
+
+  services.wyoming.faster-whisper = {
+    servers = {
+      "en" = {
+        enable = true;
+        # see https://github.com/rhasspy/rhasspy3/blob/master/programs/asr/faster-whisper/script/download.py
+        model = "tiny-int8";
+        language = "de";
+        uri = "tcp://0.0.0.0:10300";
+        device = "cpu";
+      };
+    };
+  };
+
+  # needs access to /proc/cpuinfo
+  systemd.services."wyoming-faster-whisper-en".serviceConfig.ProcSubset = lib.mkForce "all";
 }
