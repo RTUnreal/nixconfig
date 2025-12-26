4b069faa475cbf135b4d5c885391d0e4aeff40d4
RTUnreal <unreal@rtinf.net>
2025-12-26 21:37:21 +0100

    1systems/overte-demo: fix

--- 1systems/overte-demo/config.nix
+++ 1systems/overte-demo/config.nix
@@ -6,8 +6,6 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./retiolum-cfg.nix
-    ../../2configs/rocm.nix
   ];
   rtinf = {
     base = {
