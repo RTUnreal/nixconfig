ee6e7735f2da968631a93579488daa2fe52bea46
RTUnreal <unreal@rtinf.net>
2025-12-26 22:40:06 +0100

    1systems/overte-demo: fix 8

--- 1systems/overte-demo/hardware-configuration.nix
+++ 1systems/overte-demo/hardware-configuration.nix
@@ -2,6 +2,14 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+  ];
   boot.kernelModules = [ "kvm-amd" ];
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/0e4e8a12-9890-4f12-a2f8-d66ca596c5bf";
