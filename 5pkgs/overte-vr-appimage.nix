ef5e96c30dfce3e566190294ced854929c1d4180
RTUnreal <unreal@rtinf.net>
2026-04-21 19:34:21 +0200

    5pkgs/overte-vr-appimage: update

--- 5pkgs/overte-vr-appimage.nix
+++ 5pkgs/overte-vr-appimage.nix
@@ -4,10 +4,10 @@
 }:
 appimageTools.wrapType2 rec {
   pname = "overte-vr";
-  version = "2025.12.1";
+  version = "2026.04.1";
   src = fetchurl {
     url = "https://public.overte.org/build/overte/release/${version}/Overte-${version}-x86_64.AppImage";
     #url = "https://public.overte.org/build/overte/release-candidate/${version}/Overte-${version}-x86_64.AppImage";
-    hash = "sha256-FU8z1JS+1/r5QNzH9iXetas5M9fzj2kOCOANHVBbCUA=";
+    hash = "sha256-3Dn1tGlKHEjPsUVKiSL2ol+k21ggv5NxE+v0CGqpkUU=";
   };
 }
